0242e7c8f04c67a8d892c58f83d4d8c39a2b6fc9
Abdulhakim Salum <42657558+moson93@users.noreply.github.com>
2019-07-23 18:39:57 +0530

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,16 @@
+MAILGUN_API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx-xxxxxxxx-xxxxxxx'
+  
+SANDBOX_URL= 'sandboxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.mailgun.org'
+
+SENDER_EMAIL = 'xxxxxxxxxxx@xxxxxx.xxxx'
+
+RECIPIENT_EMAIL = 'xxxxxxxxxxxxxx@xxxxxxxxxx.xxx'
+
+API_KEY = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx'
+
+DEVICE_ID = 'BOLTxxxxxxxx'
+
+FRAME_SIZE = 10
+
+MUL_FACTOR = 6
+
